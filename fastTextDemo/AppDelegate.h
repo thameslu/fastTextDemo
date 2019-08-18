39b8d210544b4806f5af137babd1cf8ba8213fe0
thameslu <luzitian@foxmail.com>
2019-08-18 22:15:30 +0800

    Initial Commit

---
+++ fastTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fastTextDemo
+//
+//  Created by thameslu on 2019/8/18.
+//  Copyright © 2019年 thames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fastTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fastTextDemo
+//
+//  Created by thameslu on 2019/8/18.
+//  Copyright © 2019年 thames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
